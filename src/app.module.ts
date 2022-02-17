ecb1a925731e943988999cb361fde7b8d28ba652
scaabel <bidayuhcoder@protonmail.com>
2022-02-17 23:51:21 +0800

    feat: import `coffees` modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
+    imports: [CoffeesModule],
     controllers: [AppController],
     providers: [AppService],
 })
