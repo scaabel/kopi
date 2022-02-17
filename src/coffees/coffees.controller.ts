a466f89f338da1a4b36adb0b9106f9fe47b33253
scaabel <bidayuhcoder@protonmail.com>
2022-02-17 23:48:45 +0800

    feat(coffees): add controllers for CRUD endpoints

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,43 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) {}
+
+    @Get('flavors')
+    findAll() {
+        return this.coffeesService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        console.log(typeof id);
+        return this.coffeesService.findOne('' + id);
+    }
+
+    @Post()
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        return this.coffeesService.create(createCoffeeDto);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCoffeeDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.coffeesService.remove(id);
+    }
+}
