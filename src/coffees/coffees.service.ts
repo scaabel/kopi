7bce1c65df986079941c6e35fc90da6689eb8ac7
scaabel <bidayuhcoder@protonmail.com>
2022-02-19 23:21:18 +0800

    feat(coffees): implement event  for coffee recommendation

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { Event } from '../event/entities/event.entity';
 
 @Injectable()
 export class CoffeesService {
@@ -13,7 +14,8 @@ export class CoffeesService {
         @InjectRepository(Coffee)
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
-        private readonly flavorRepository: Repository<Flavor>
+        private readonly flavorRepository: Repository<Flavor>,
+        private readonly connection: Connection
     ) {}
 
     findAll(paginationQuery: PaginationQueryDto) {
@@ -73,6 +75,31 @@ export class CoffeesService {
     async remove(id: string) {
         const coffee = await this.findOne(id);
         return this.coffeeRepository.remove(coffee);
+    }
+
+    async recommendCoffee(coffee: Coffee) {
+        const queryRunner = this.connection.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+        try {
+            coffee.recommendation++;
+
+            const recommendEvent = new Event();
+            recommendEvent.name = 'recommend_coffee';
+            recommendEvent.type = 'coffee';
+            recommendEvent.payload = { coffeeId: coffee.id };
+
+            await queryRunner.manager.save(coffee);
+            await queryRunner.manager.save(recommendEvent);
+
+            await queryRunner.commitTransaction();
+        } catch (e) {
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+        }
     }
 
     private async firstOrCreateFlavorByName(name: string): Promise<Flavor> {
