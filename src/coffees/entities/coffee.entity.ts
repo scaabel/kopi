2f56c8520ff234a2973a8d7b19b8f4695dd76d2d
scaabel <bidayuhcoder@protonmail.com>
2022-02-19 21:59:51 +0800

    feat(coffees): add many to many relationship with flavors

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    JoinTable,
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+    ManyToMany,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
 
 @Entity()
 export class Coffee {
@@ -11,6 +18,7 @@ export class Coffee {
     @Column()
     brand: string;
 
-    @Column('json', { nullable: true })
+    @JoinTable()
+    @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
     flavors: string[];
 }
