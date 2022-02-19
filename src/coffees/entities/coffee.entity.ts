410724649bc601c7fa6098708bd7b864bdd17886
scaabel <bidayuhcoder@protonmail.com>
2022-02-19 22:11:55 +0800

    feat(coffees): add cascade to coffee entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -19,6 +19,8 @@ export class Coffee {
     brand: string;
 
     @JoinTable()
-    @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
+    @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, {
+        cascade: true,
+    })
     flavors: string[];
 }
