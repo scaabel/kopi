f587e036bf1602575c4aec060752741414b0f840
scaabel <bidayuhcoder@protonmail.com>
2022-02-19 22:53:51 +0800

    feat(coffees): add create and update flavors in respective apis

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,12 +4,15 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
     constructor(
         @InjectRepository(Coffee)
-        private readonly coffeeRepository: Repository<Coffee>
+        private readonly coffeeRepository: Repository<Coffee>,
+        @InjectRepository(Flavor)
+        private readonly flavorRepository: Repository<Flavor>
     ) {}
 
     findAll() {
@@ -28,16 +31,34 @@ export class CoffeesService {
         return coffee;
     }
 
-    create(createCoffeeDto: CreateCoffeeDto) {
-        const coffee = this.coffeeRepository.create(createCoffeeDto);
+    async create(createCoffeeDto: CreateCoffeeDto) {
+        const flavors = await Promise.all(
+            createCoffeeDto.flavors.map((name) =>
+                this.firstOrCreateFlavorByName(name)
+            )
+        );
+
+        const coffee = this.coffeeRepository.create({
+            ...createCoffeeDto,
+            flavors,
+        });
 
         return this.coffeeRepository.save(coffee);
     }
 
     async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+        const flavors =
+            updateCoffeeDto.flavors &&
+            (await Promise.all(
+                updateCoffeeDto.flavors.map((name) =>
+                    this.firstOrCreateFlavorByName(name)
+                )
+            ));
+
         const coffee = await this.coffeeRepository.preload({
             id: id,
             ...updateCoffeeDto,
+            flavors,
         });
 
         if (!coffee) throw new NotFoundException(`Coffee #${id} not found`);
@@ -48,5 +69,14 @@ export class CoffeesService {
     async remove(id: string) {
         const coffee = await this.findOne(id);
         return this.coffeeRepository.remove(coffee);
+    }
+
+    private async firstOrCreateFlavorByName(name: string): Promise<Flavor> {
+        const existingFlavor = this.flavorRepository.findOne({ name: name });
+        if (existingFlavor) {
+            return existingFlavor;
+        }
+
+        return this.flavorRepository.create({ name });
     }
 }
