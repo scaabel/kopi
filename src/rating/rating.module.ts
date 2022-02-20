c0652a0c0fc4da6f31f111101f105b30b9d71c53
scaabel <bidayuhcoder@protonmail.com>
2022-02-20 23:27:55 +0800

    feat(rating): use database module for db connection

--- src/rating/rating.module.ts
+++ src/rating/rating.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RatingService } from './rating.service';
+import { CoffeesModule } from '../coffees/coffees.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  providers: [RatingService]
+    imports: [
+        DatabaseModule.register({
+            type: 'postgres',
+            host: 'localhost',
+            password: 'secret',
+            port: 5432,
+            database: 'testing_env',
+        }),
+        CoffeesModule,
+    ],
+    providers: [RatingService],
 })
 export class RatingModule {}
