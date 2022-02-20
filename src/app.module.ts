0277dd2ee36da5f8c901cc3830ea9bd60d4d46dd
scaabel <bidayuhcoder@protonmail.com>
2022-02-20 23:37:16 +0800

    build: add validation schema for .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RatingModule } from './rating/rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                DATABASE_HOST: Joi.required(),
+                DATABASE_PORT: Joi.number().default(5432),
+                DATABASE_USER: Joi.required(),
+                DATABASE_PASSWORD: Joi.required(),
+                DATABASE_NAME: Joi.required(),
+                DATABASE_AUTO_LOAD_ENTITIES: Joi.bool().default(true),
+                DATABASE_SYNCHRONIZE: Joi.bool().default(false),
+            }),
+        }),
         CoffeesModule,
         TypeOrmModule.forRoot({
             type: 'postgres',
