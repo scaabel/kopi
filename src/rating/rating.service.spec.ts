554cd070c0956f51377229a70edc4e300ecc0e68
scaabel <bidayuhcoder@protonmail.com>
2022-02-20 23:28:16 +0800

    chore(rating): cleanup files

--- src/rating/rating.service.spec.ts
+++ src/rating/rating.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RatingService } from './rating.service';
 
 describe('RatingService', () => {
-  let service: RatingService;
+    let service: RatingService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RatingService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [RatingService],
+        }).compile();
 
-    service = module.get<RatingService>(RatingService);
-  });
+        service = module.get<RatingService>(RatingService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
--- src/rating/rating.service.ts
+++ src/rating/rating.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { CoffeesService } from '../coffees/coffees.service';
 
 @Injectable()
-export class RatingService {}
+export class RatingService {
+    constructor(private readonly coffeesService: CoffeesService) {}
+}
